3789c3bbc7f41b8367c2db86748d09b4573305f5
tayler.higgins2 <tayler.higgins2@600900B211DE414>
2018-02-28 11:57:12 +1100

    Started Asteroids Porject

---
+++ Assets/~Asteroids/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+
+    public class PlayerController : MonoBehaviour
+    {
+        public Moving movement;
+
+        // Update is called once per frame
+        void Update()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            float inputH = Input.GetAxis("Horizontal");
+            if (inputV > 0)
+            {
+                movement.Accelerate(transform.up);
+            }
+            movement.Rotate(inputH);
+
+        }
+    }
+}
---
+++ Assets/~Asteroids/Scripts/ScreenWrap.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class ScreenWrap : MonoBehaviour
+
+    {
+        private SpriteRenderer spriteRenderer;
+        private Bounds camBounds;
+        private float camWidth;
+        private float camHeight;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        void UpdateCameraBounds()
+        {
+            Camera cam = Camera.main;
+            camHeight = 2f * cam.orthographicSize;
+            camWidth = camHeight * cam.aspect;
+            camBounds = new Bounds(cam.transform.position, new Vector2(camWidth, camHeight));
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            UpdateCameraBounds();
+            Vector3 pos = transform.position;
+            Vector3 size = spriteRenderer.bounds.size;
+            float halfWidth = size.x / 2f;
+            float halfHeight = size.y / 2f;
+            if (pos.x + halfWidth < camBounds.min.x)
+            {
+                pos.x = camBounds.max.x + halfWidth;
+            }
+            if (pos.x - halfWidth > camBounds.max.x)
+            {
+                pos.x = camBounds.min.x - halfWidth;
+            }
+            if (pos.y + halfHeight < camBounds.min.y)
+            {
+                pos.y = camBounds.max.y + halfHeight;
+            }
+            if (pos.y - halfHeight > camBounds.max.y)
+            {
+                pos.y = camBounds.min.y - halfHeight;
+            }
+
+            transform.position = pos;
+        }
+    }
+}
